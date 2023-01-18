17e6ca398c5f130a9fb26b493d76236c5809e286
SebiC98 <sebastiancioata98@gmail.com>
2023-01-18 11:24:22 +0200

    Base project

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Sebastian Cioată on 18.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
