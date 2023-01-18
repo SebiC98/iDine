9c5c3ba54b5b0e154a33d21f1e5f565717114e19
SebiC98 <sebastiancioata98@gmail.com>
2023-01-18 14:00:16 +0200

    Menu orders

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -17,13 +17,20 @@ struct ContentView: View {
                 ForEach(menu){ section in
                     Section(section.name){
                         ForEach(section.items){ item in
-                            ItemRow(item: item)
+                            NavigationLink(value: item){
+                                ItemRow(item: item)
+                            }
+                            
                         }
                     }
+                    
                     
                 }
                 
             }
+            .navigationDestination(for: MenuItem.self, destination: { item in
+                ItemDetail(item: item)
+            })
             .navigationTitle("Menu")
             .listStyle(.grouped)
         }
---
+++ iDine/OrderView.swift
@@ -0,0 +1,41 @@
+//
+//  OrderView.swift
+//  iDine
+//
+//  Created by Sebastian Cioată on 18.01.2023.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    
+    @EnvironmentObject var order: Order
+    
+    var body: some View {
+        NavigationStack{
+            List{
+                Section{
+                    ForEach(order.items){item in
+                        HStack{
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price)")
+                        }
+                    }
+                }
+                Section{
+                    NavigationLink("Place order"){
+                        Text("Check out")
+                    }
+                }
+            }
+            .navigationTitle("Order")
+        }
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderView()
+    }
+}
